08ca57765141bee6a86b5b5e3af106e4c79feabe
Your Name <julian.sonido@gmail.com>
2023-01-18 11:13:05 -0300

    siguiendo con el deploy en Render

--- src/db.js
+++ src/db.js
@@ -12,6 +12,7 @@ const {
 }); */
 
 
+
 const sequelize = new Sequelize(DB_DEPLOY_RENDER, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
