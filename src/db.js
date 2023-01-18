442b7f3e21c21e8c5fae1323726c3c990ca96ebd
Your Name <julian.sonido@gmail.com>
2023-01-18 11:34:53 -0300

    sigo con el deploy en Render

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY_RENDER
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
@@ -12,8 +12,7 @@ const {
 }); */
 
 
-
-const sequelize = new Sequelize(DB_DEPLOY_RENDER, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
